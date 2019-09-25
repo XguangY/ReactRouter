6f1885d0bcdd28394af333a5e3034a00c62eeb71
theone <xuguangyue@dtrobo.com>
2019-09-25 18:31:51 +0800

    伪动态路由

--- demo02/src/AppRouter.js
+++ demo02/src/AppRouter.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import { BrowserRouter  as Router, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Index from './Pages/Index'
 import Video from './Pages/video/Video'
 import WorkPlace from './Pages/workPlace/WorkPlace'
 import './index.css'
 function AppRouter() {
+    let routeConfig = [
+        { path: '/', title: '博客首页', exact: true, component: Index },
+        { path: '/video/', title: '视频教程1', exact: false, component: Video },
+        { path: '/workplace/', title: '职场技能', exact: false, component: WorkPlace }
+    ]
     return (
         <Router>
             <div className="mainDiv">
                 <div className="leftNav">
                     <h3>一级导航</h3>
                     <ul>
-                        <li><Link to="/">博客首页</Link></li>
-                        <li><Link to="/video/reactpage">视频教程</Link></li>
-                        <li><Link to="/workplace/money">职场技能</Link></li>
+                        {
+                            routeConfig.map((item, index) => {
+                                return (<li key={index}> <Link to={item.path}>{item.title}</Link> </li>)
+                            })
+                        }
                     </ul>
                 </div>
                 <div className="rightMain">
-                    <Route path="/" exact component={Index}></Route>
-                    <Route path="/video" component={Video}></Route>
-                    <Route path="/workplace" component={WorkPlace}></Route>
+                    {
+                        routeConfig.map((item, index) => {
+                            return (<Route key={index} exact={item.exact} path={item.path} component={item.component} />)
+                        })
+                    }
                 </div>
             </div>
         </Router>
